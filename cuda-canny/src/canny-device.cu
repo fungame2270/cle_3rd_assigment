49ead40deed2d8697d74cc01232581db25c478fc
fungame2270 <tomasvictal@gmail.com>
2025-05-19 11:35:14 +0100

    first commit

---
+++ cuda-canny/src/canny-device.cu
@@ -0,0 +1,11 @@
+
+// CLE 24'25
+
+// canny edge detector code to run on the GPU
+void cannyDevice( const int *h_idata, const int w, const int h,
+                 const int tmin, const int tmax,
+                 const float sigma,
+                 int * h_odata)
+{
+    //TODO: insert your code here
+}
